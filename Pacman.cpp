a85cf747281760f4f082038e6ade88f66031056f
Nguyễn Hoàng Gia Bảo <giabao441998@gmail.com>
2019-03-27 20:28:01 +0700

    Done level 4
    Need to fix the draw function

--- Pacman.cpp
+++ Pacman.cpp
@@ -1,70 +1,74 @@
-#include<cstdio>
 #include<vector>
 #include<deque>
 #include<iostream>
 #include<Windows.h>
-#include<stdlib.h>
+#include<cstdlib>
 #include<fstream>
 #include<string>
-#include<string.h>
-#include<cstring>
+#include<string>
+#include<set>
 using namespace std;
 typedef pair<int,int> position;
 struct element1
 {
-	short state; position pre;
+	short state; position pre; short anotherState;
 };
 struct element2
 {
 	int x,y,len;
 };
 typedef vector<vector<element1> > matrix;
+typedef pair<short,short> couple;
 const short foodPoint=10;
-const pair<short,short> direction[4]={{-1,0},{0,1},{1,0},{0,-1}};
-const int waitTime=500;
-int N,M,gamePoint; matrix original(1); position pacman; deque<position> food,path; deque<element2> q;
+const couple direction[4]={{-1,0},{0,1},{1,0},{0,-1}};
+const int waitTime=1000;
+int N,M,gamePoint; matrix original(1); position pacman; deque<position> path; short level; vector<position> monster; set<position> food; vector<vector<position> > way;
 void inp()
 {
-	freopen("map.txt","r",stdin);
-	scanf("%d%d",&N,&M);
+	ifstream f("map.txt");
+	f>>N>>M;
 	int i,j;
 	for(i=1;i<=N;++i)
 	{
-		original.push_back({{1,{0,0}}});
+		original.push_back({{1,{0,0},-1}});
 		for(j=1;j<=M;++j)
 		{
-			original[i].push_back({0,{0,0}});
-			scanf("%hd",&original[i][j].state);
+			original[i].push_back({0,{0,0},-1});
+			f>>original[i][j].state;
+			switch (original[i][j].state)
+			{
+				case 2: food.insert({i,j}); break;
+				case 3: monster.push_back({i,j}); original[i][j].anotherState=0;
+			}
 		}
-		original[i].push_back({1,{0,0}});
+		original[i].push_back({1,{0,0},-1});
 	}
 	original.push_back({});
 	for(i=M+1;i>=0;--i)
 	{
-		original[0].push_back({1,{0,0}});
-		original.back().push_back({1,{0,0}});
+		original[0].push_back({1,{0,0},-1});
+		original.back().push_back({1,{0,0},-1});
 	}
-	scanf("%d%d",&pacman.first,&pacman.second);
-	fclose(stdin);
+	f>>pacman.first>>pacman.second;
+	f.close();
 	++pacman.first; ++pacman.second;
+	do
+	{
+		cout<<"Choose level (1-4): ";
+		cin>>level;
+	}
+	while (level<1 || level>4);
 }
 void level1_2()
 {
+	deque<element2> q;
 	q.push_back({pacman.first,pacman.second,0});
-	matrix a=original;
-	int i,j;
-	for(i=1;i<=N;++i)
-	for(j=1;j<=M;++j)
-	if (a[i][j].state==2)
-	{
-		food.push_back({i,j});
-		goto label1;
-	}
-	label1:
-	if (pacman==food[0]) gamePoint=foodPoint;
+	matrix a=original; position tmp=*food.begin();
+	if (pacman==tmp) gamePoint=foodPoint;
 	else
 	{
 		gamePoint=0; a[pacman.first][pacman.second].state=1;
+		short i;
 		do
 		{
 			for(i=0;i<4;++i)
@@ -75,7 +79,7 @@ void level1_2()
 				if (a[q.back().x][q.back().y].state==2)
 				{
 					gamePoint=foodPoint-q.back().len;
-					goto label2;
+					goto label;
 				}
 				a[q.back().x][q.back().y].state=1;
 			}
@@ -83,35 +87,155 @@ void level1_2()
 		}
 		while (!q.empty() && q[0].len<foodPoint-1);
 	}
-	label2:
-	while (a[food[0].first][food[0].second].pre!=position(0,0))
+	label:
+	while (a[tmp.first][tmp.second].pre!=position(0,0))
 	{
-		path.push_front(food[0]);
-		food[0]=a[food[0].first][food[0].second].pre;
+		path.push_front(tmp);
+		tmp=a[tmp.first][tmp.second].pre;
 	}
 	path.push_front(pacman);
 }
-void dfs(const position& cur,matrix& a,int& comeback)
+bool exist(const position& cur,const matrix& a,const short& k)
+{
+	int i,j;
+	switch (k)
+	{
+		case 0:
+			for(i=1;i<cur.first;++i)
+			for(j=1;j<=M;++j)
+			if (a[i][j].state==2) return true;
+			return false;
+		case 1:
+			for(i=1;i<=N;++i)
+			for(j=cur.second+1;j<=M;++j)
+			if (a[i][j].state==2) return true;
+			return false;
+		case 2:
+			for(i=cur.first+1;i<=N;++i)
+			for(j=1;j<=M;++j)
+			if (a[i][j].state==2) return true;
+			return false;
+		case 3:
+			for(i=1;i<=N;++i)
+			for(j=1;j<cur.second;++j)
+			if (a[i][j].state==2) return true;
+			return false;
+	}
+}
+bool dfs(const position& cur,matrix& a,int& comeback)
 {
 	gamePoint-=comeback+1; comeback=0;
-	if (a[cur.first][cur.second].state==2) gamePoint+=foodPoint;
-	a[cur.first][cur.second].state=1;
-	path.insert(path.end(),food.begin(),food.end());
-	path.push_back(cur);
-	food.clear();
-	for(short i=0;i<4;++i)
-	if (a[cur.first+direction[i].first][cur.second+direction[i].second].state==0 || a[cur.first+direction[i].first][cur.second+direction[i].second].state==2)
+	if (a[cur.first][cur.second].state==2)
 	{
-		dfs({cur.first+direction[i].first,cur.second+direction[i].second},a,comeback);
-		food.push_back(cur);
+		gamePoint+=foodPoint;
+		food.erase(cur);
+	}
+	path.insert(path.end(),monster.begin(),monster.end());
+	path.push_back(cur);
+	if (food.empty()) return true;
+	a[cur.first][cur.second].state=1;
+	monster.clear();
+	vector<couple> direction_; short i;
+	for(i=0;i<4;++i)
+	if (exist(cur,a,i)) direction_.push_back(direction[i]);
+	else monster.push_back(direction[i]);
+	direction_.insert(direction_.end(),monster.begin(),monster.end());
+	monster.clear();
+	for(i=0;i<4;++i)
+	if (a[cur.first+direction_[i].first][cur.second+direction_[i].second].state==0 || a[cur.first+direction_[i].first][cur.second+direction_[i].second].state==2)
+	{
+		if (dfs({cur.first+direction_[i].first,cur.second+direction_[i].second},a,comeback)) return true;
+		monster.push_back(cur);
 	}
 	++comeback;
+	return false;
 }
 void level3()
 {
 	matrix a=original; int comeback=-1;
 	gamePoint=0;
+	monster.clear();
 	dfs(pacman,a,comeback);
+}
+short bfsPacman(matrix& a)
+{
+	deque<element2> q; short i;
+	a[pacman.first][pacman.second].state=1;
+	for(i=0;i<4;++i)
+	switch (a[pacman.first+direction[i].first][pacman.second+direction[i].second].state)
+	{
+		case 0: q.push_back({pacman.first+direction[i].first,pacman.second+direction[i].second,i}); break;
+		case 2: return i;
+	}
+	while (!q.empty())
+	{
+		for(i=0;i<4;++i)
+		switch (a[q[0].x+direction[i].first][q[0].y+direction[i].second].state)
+		{
+			case 0: q.push_back({q[0].x+direction[i].first,q[0].y+direction[i].second,q[0].len}); a[q.back().x][q.back().y].state=1; break;
+			case 2: return q[0].len;
+		}
+		q.pop_front();
+	}
+	return -1;
+}
+short bfsMonster(const position& m,matrix& a)
+{
+	deque<element2> q; short i;
+	a[m.first][m.second].state=1;
+	for(i=0;i<4;++i)
+	switch (a[m.first+direction[i].first][m.second+direction[i].second].state)
+	{
+		case 0: q.push_back({m.first+direction[i].first,m.second+direction[i].second,i}); break;
+		case 2: return i;
+	}
+	while (!q.empty())
+	{
+		for(i=0;i<4;++i)
+		if (a[q[0].x+direction[i].first][q[0].y+direction[i].second].state!=1)
+		{
+			q.push_back({q[0].x+direction[i].first,q[0].y+direction[i].second,q[0].len});
+			if (position(q.back().x,q.back().y)==pacman) return q[0].len;
+			a[q.back().x][q.back().y].state=1;
+		}
+		q.pop_front();
+	}
+	return -1;
+}
+bool moveMonster(matrix& a)
+{
+	short j;
+	for(int i=0;i<monster.size();++i)
+	if ((j=bfsMonster(monster[i],a))>=0)
+	{
+		monster[i].first+=direction[j].first; monster[i].second+=direction[j].second;
+		if (monster[i]==pacman) return false;
+		if (original[monster[i].first][monster[i].second].state!=3)
+		{
+			original[monster[i].first][monster[i].second].anotherState=original[monster[i].first][monster[i].second].state;
+			original[monster[i].first][monster[i].second].state=3;
+		}
+		original[monster[i].first-direction[j].first][monster[i].second-direction[j].second].state=original[monster[i].first-direction[j].first][monster[i].second-direction[j].second].anotherState;
+		way[i].push_back(monster[i]);
+	}
+	return true;
+}
+void level4()
+{
+	gamePoint=0;
+	path.push_back(pacman);
+	int i; matrix a=original;
+	for(i=0;i<monster.size();++i) way.push_back({monster[i]});
+	while (!food.empty() && (i=bfsPacman(a))>=0)
+	{
+		pacman.first+=direction[i].first; pacman.second+=direction[i].second;
+		if (!moveMonster(a)) break;
+		if (original[pacman.first][pacman.second].state==2)
+		{
+			gamePoint+=foodPoint; original[pacman.first][pacman.second].state=0;
+		}
+		path.push_back(pacman);
+	}
 }
 void out()
 {
@@ -160,7 +284,7 @@ void draw()
 					cout << "#";
 					break;
 				case 2:
-					if (i == path[k].first&&j == path[k].second) {
+					if (i == path[k].first && j == path[k].second) {
 						SetConsoleTextAttribute(h, 14 | FOREGROUND_INTENSITY);
 						cout << "x";
 						original[i][j].state=0;
@@ -175,7 +299,7 @@ void draw()
 					cout << "!";
 					break;
 				default:
-					if (i == path[k].first&&j == path[k].second) {
+					if (i == path[k].first && j == path[k].second) {
 						SetConsoleTextAttribute(h, 14 | FOREGROUND_INTENSITY);
 						cout << "x";
 					}
@@ -186,21 +310,24 @@ void draw()
 				}
 			cout << endl;
 		}
-		/*
 		SetConsoleTextAttribute(h, 13 | FOREGROUND_INTENSITY);
 		cout << "Path: ";
 		for (int i = 0; i <= k; i++) {
 			cout << "(" << path[i].first - 1 << ", " << path[i].second - 1 << ") ";
 		}
-		SetConsoleCursorPosition(h, { 0,0 });
-		*/
 		Sleep(waitTime);
 	}
 }
 int main()
 {
 	inp();
-	level3();
+	switch (level)
+	{
+		case 3: level3(); break;
+		case 4: level4(); break;
+		default:
+			level1_2();
+	}
 	out();
 	draw();
 }
